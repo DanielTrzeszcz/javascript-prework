3e422720a3a99e03c611c6cd9dbaf461994ad2db
Daniel Trzeszczkowski <daniel_trzeszczkowski@o2.pl.>
2025-02-27 12:08:35 +0100

    Musze isc do pracy

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -11,6 +11,7 @@ if(randomNumber == 1){
 } else if (playerInput == '3') {
   computerMove = 'nożyce';
 }
+  */
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -18,15 +19,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == '1') {
+/*if (playerInput == '1') {
   playerMove = 'kamień';
 } else if (playerInput == '2') {
   playerMove = 'papier';
 } else if (playerInput == '3') {
   playerMove = 'nożyce';
-}
+}*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
