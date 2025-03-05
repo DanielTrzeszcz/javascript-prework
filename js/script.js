e7287a88eab589c082be3eb2a166815ee85fe311
Daniel Trzeszczkowski <daniel_trzeszczkowski@o2.pl.>
2025-03-05 09:28:25 +0100

    Poprawa linijka 37-50

--- js/script.js
+++ js/script.js
@@ -1,54 +1,45 @@
- {
-    const getMoveName = (moveNumber) => {
-      if (moveNumber === 1) return 'kamień';
-      if (moveNumber === 2) return 'papier';
-      if (moveNumber === 3) return 'nożyce';
-      return 'nieznany ruch';
-    };
+{
+  const getMoveName = (moveNumber) => {
+    if (moveNumber === 1) return 'kamień';
+    if (moveNumber === 2) return 'papier';
+    if (moveNumber === 3) return 'nożyce';
+    return 'nieznany ruch';
+  };
 
-    const displayResult = (argComputerMove, argPlayerMove) => {
-      if (argComputerMove === argPlayerMove) {
-        printMessage('Remis!');
-      } else if (
-        (argComputerMove === 'kamień' && argPlayerMove === 'papier') ||
-        (argComputerMove === 'papier' && argPlayerMove === 'nożyce') ||
-        (argComputerMove === 'nożyce' && argPlayerMove === 'kamień')
-      ) {
-        printMessage('Ty wygrywasz!');
-      } else {
-        printMessage('Przegrywasz!');
-      }
-    };
+  const displayResult = (argComputerMove, argPlayerMove) => {
+    if (argComputerMove === argPlayerMove) {
+      printMessage('Remis!');
+    } else if (
+      (argComputerMove === 'kamień' && argPlayerMove === 'papier') ||
+      (argComputerMove === 'papier' && argPlayerMove === 'nożyce') ||
+      (argComputerMove === 'nożyce' && argPlayerMove === 'kamień')
+    ) {
+      printMessage('Ty wygrywasz!');
+    } else {
+      printMessage('Przegrywasz!');
+    }
+  };
 
-    const playGame = (playerInput) => {
-      clearMessages();
-  
-      const randomNumber = Math.floor(Math.random() * 3 + 1);
-      const computerMove = getMoveName(randomNumber);
-      const playerMove = getMoveName(playerInput);
-  
-      printMessage(`Mój ruch to: ${computerMove}`);
-      printMessage(`Twój ruch to: ${playerMove}`);
-  
-      displayResult(computerMove, playerMove);
-    };
-  
+  const playGame = (playerInput) => {
+    clearMessages();
 
-// Pobranie przycisków
-let playRock = document.getElementById('play-rock');
-let playPaper = document.getElementById('play-paper');
-let playScissors = document.getElementById('play-scissors');
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerMove = getMoveName(randomNumber);
+    const playerMove = getMoveName(playerInput);
 
-// Przypisanie funkcji do guzików
-playRock.addEventListener('click', function () {
-  playGame(1);
-});
+    printMessage(`Mój ruch to: ${computerMove}`);
+    printMessage(`Twój ruch to: ${playerMove}`);
 
-playPaper.addEventListener('click', function () {
-  playGame(2);
-});
+    displayResult(computerMove, playerMove);
+  };
 
-playScissors.addEventListener('click', function () {
-  playGame(3);
-});
+  {
+    const playRock = document.getElementById('play-rock');
+    const playPaper = document.getElementById('play-paper');
+    const playScissors = document.getElementById('play-scissors');
+
+    playRock.addEventListener('click', () => playGame(1));
+    playPaper.addEventListener('click', () => playGame(2));
+    playScissors.addEventListener('click', () => playGame(3));
+  }
 }
